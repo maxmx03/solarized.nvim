a5b55172071ac05072abbb6bee5825a6a6ec0d12
Max Miliano <milianordelcanto@gmail.com>
2022-05-08 14:57:28 -0300

    update whickKey bg color

--- lua/solarized/Whichkey.lua
+++ lua/solarized/Whichkey.lua
@@ -1,9 +1,10 @@
 local Whichkey = {
-		WhichKey = {fg = C.purple, },
-		WhichKeySeperator = {fg = C.green, },
-		WhichKeyGroup = {fg = C.cyan, },
-		WhichKeyDesc = {fg = C.blue, },
-		WhichKeyFloat = {bg = C.dark, },
+  WhichKey = { fg = C.purple },
+  WhichKeySeperator = { fg = C.green },
+  WhichKeyGroup = { fg = C.cyan },
+  WhichKeyDesc = { fg = C.blue },
+  WhichKeyFloat = { bg = C.alt_bg },
 }
 
 return Whichkey
+
