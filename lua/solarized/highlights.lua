d1e8f56838ea93c05738092277d52f1fb80eefc0
Max Miliano <milianordelcanto@gmail.com>
2022-05-17 22:09:29 -0300

    fix solarized highlights

--- lua/solarized/highlights.lua
+++ lua/solarized/highlights.lua
@@ -19,7 +19,7 @@ local highlights = {
   LineNr = { fg = C.context },
   FloatBoder = { fg = C.gray, bg = C.alt_bg },
   Whitespace = { fg = C.bg },
-  VertSplit = { fg = C.bg, bg = C.fg },
+  VertSplit = { fg = C.fg, bg = C.bg },
   CursorLine = { bg = C.dark },
   CursorColumn = { bg = C.dark },
   ColorColumn = { bg = C.dark },
