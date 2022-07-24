cfc6b14603d515f366c6f802b678bdf21d2d3a97
Max Miliano <milianordelcanto@gmail.com>
2022-07-24 12:03:01 -0300

    update utils and base highlights

--- lua/solarized/utils.lua
+++ lua/solarized/utils.lua
@@ -2,7 +2,19 @@ local utils = {}
 
 function utils.set_highlights(highlight_groups)
   for group_name, group in pairs(highlight_groups) do
-    vim.api.nvim_set_hl(0, group_name, { fg = group.fg or 'NONE', bg = group.bg or 'NONE' })
+    local val = {
+      fg = group.fg or 'NONE',
+      bg = group.bg or 'NONE',
+      sp = group.sp or nil,
+      bold = group.bold or false,
+      italic = group.italic or false,
+      reverse = group.reverse or false,
+      underline = group.underline or false,
+      undercurl = group.undercurl or false,
+      link = group.link or nil,
+    }
+
+    vim.api.nvim_set_hl(0, group_name, val)
   end
 end
 
