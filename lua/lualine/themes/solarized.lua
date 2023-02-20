9b8a82121d50e61f5336437f1d7ec2e51f091930
Max Miliano <milianordelcanto@gmail.comr>
2023-02-20 12:49:02 -0300

    fix lualine

--- lua/lualine/themes/solarized.lua
+++ lua/lualine/themes/solarized.lua
@@ -8,7 +8,7 @@ local M = {
     a = { fg = darken(colors.blue, 50), bg = colors.blue, gui = 'bold' },
     b = { fg = colors.bg_alt, bg = colors.content },
     c = { fg = colors.content, bg = solarized:is_transparent(colors.bg_alt) },
-    z = { fg = darken(colors.primary, 50), bg = colors.blue },
+    z = { fg = darken(colors.blue, 50), bg = colors.blue },
   },
   insert = {
     a = { fg = darken(colors.green, 50), bg = colors.green },
