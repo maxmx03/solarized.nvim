d310bf291b9c372b2fda41054502f913c1a572c4
Max Miliano <milianordelcanto@gmail.com>
2023-03-18 13:26:20 -0300

    update: semantic highlight for neovim and vscode themes

--- lua/solarized/colorscheme.lua
+++ lua/solarized/colorscheme.lua
@@ -93,27 +93,12 @@ function Colorscheme.load()
 end
 
 function Colorscheme:apply_semantic_hl()
-  local function show_unconst_caps(args)
-    local token = args.data.token
-    if token.type ~= 'variable' or token.modifiers.readonly then
-      return
-    end
-
-    local text = vim.api.nvim_buf_get_text(args.buf, token.line, token.start_col, token.line, token.end_col, {})[1]
-
-    if text ~= string.upper(text) then
-      return
-    end
-
-    vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, '@constant')
-  end
-
-  local set_hl = function(name, val)
-    vim.api.nvim_set_hl(0, name, val)
-  end
-
   vim.api.nvim_create_autocmd('Colorscheme', {
     callback = function()
+      local set_hl = function(name, val)
+        vim.api.nvim_set_hl(0, name, val)
+      end
+
       set_hl('@lsp.type.parameter', { link = '@parameter' })
       set_hl('@lsp.typemod.parameter.readyonly', { italic = true })
       set_hl('@lsp.typemod.variable.readonly', { link = '@variable.builtin' })
@@ -122,7 +107,21 @@ function Colorscheme:apply_semantic_hl()
   })
 
   vim.api.nvim_create_autocmd('LspTokenUpdate', {
-    callback = show_unconst_caps,
+    callback = function(args)
+      local token = args.data.token
+
+      if token.type ~= 'variable' or token.modifiers.readonly then
+        return
+      end
+
+      local text = vim.api.nvim_buf_get_text(args.buf, token.line, token.start_col, token.line, token.end_col, {})[1]
+
+      if text ~= string.upper(text) then
+        return
+      end
+
+      vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, '@constant')
+    end,
   })
 end
 
