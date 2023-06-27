b4ce2be776025a0b55c5c735197ec54ba6dd117a
Max Miliano <milianordelcanto@gmail.com>
2023-06-27 17:24:02 -0300

    fix: gitsign

--- lua/solarized/themes/default/gitsign.lua
+++ lua/solarized/themes/default/gitsign.lua
@@ -1,8 +1,8 @@
-return function(c)
+return function(c, config)
   local utils = require('solarized.utils')
   local set_hl = utils.set_hl
 
-  set_hl('GitSignsAdd', { fg = c.add }) -- Used for the text of 'add' signs
-  set_hl('GitSignsChange', { fg = c.warning }) -- Used for the text of 'change' signs
-  set_hl('GitSignsDelete', { fg = c.delete }) -- Used for the text of 'delete' signs
+  set_hl('GitSignsAdd', { fg = c.add, bg = c.base02 }, { transparent = config.transparent }) -- Used for the text of 'add' signs
+  set_hl('GitSignsChange', { fg = c.change, bg = c.base02 }, { transparent = config.transparent }) -- Used for the text of 'change' signs
+  set_hl('GitSignsDelete', { fg = c.delete, bg = c.base02 }, { transparent = config.delete }) -- Used for the text of 'delete' signs
 end
