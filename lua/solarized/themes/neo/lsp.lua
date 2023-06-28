a4b7dd39fc35bd48afd77584bf59768d3b49be0d
Max Del Canto <milianordelcanto@gmail.com>
2023-06-28 16:52:01 -0300

    feat: lspinlayhint

--- lua/solarized/themes/neo/lsp.lua
+++ lua/solarized/themes/neo/lsp.lua
@@ -5,11 +5,11 @@ return function()
   set_hl('LspReferenceText', { link = 'Visual' }) -- used for highlighting "text" references
   set_hl('LspReferenceRead', { link = 'Visual' }) -- used for highlighting "read" references
   set_hl('LspReferenceWrite', { link = 'Visual' }) -- used for highlighting "write" references
+  set_hl('LspInlayHint', { fg = '#8f6b00' }) -- used for highlighting inlay hints
 
   -- if you want to me to enable the highlight groups bellow, please send a screenshot for me to see how
   -- they look like or how to config for me to test.
 
-  -- set_hl('LspInlayHint') -- used for highlighting inlay hints
   -- set_hl('LspCodelens') -- Used to color the virtual text of the codelens.
   -- set_hl('LspCodeLensSeparator') -- Used to color the separator between two or more code lenses.
   -- set_hl('LspSignatureActiveParameter') -- Used to highlight the active parameter in the signature help. See
