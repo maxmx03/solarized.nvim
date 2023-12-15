fe803f15e0b58246e557d80fc1cfa09dd3203e3e
Max Miliano <milianordelcanto@gmail.com>
2023-12-15 10:55:54 -0300

    fix: inlay-hint

--- lua/solarized/themes/default/lsp.lua
+++ lua/solarized/themes/default/lsp.lua
@@ -1,12 +1,19 @@
 return function(c)
   local utils = require('solarized.utils')
+  local colortool = require('solarized.utils.colors')
+  local darken = colortool.darken
+  local lighten = colortool.lighten
   local set_hl = utils.set_hl
 
   set_hl('LspReferenceText', { link = 'Visual' }) -- used for highlighting "text" references
   set_hl('LspReferenceRead', { link = 'Visual' }) -- used for highlighting "read" references
   set_hl('LspReferenceWrite', { link = 'Visual' }) -- used for highlighting "write" references
 
-  set_hl('LspInlayHint', { fg = c.yellow })
+  if vim.o.background == 'dark' then
+    set_hl('LspInlayHint', { fg = darken(c.yellow, 20) }) -- used for highlighting inlay hints
+  else
+    set_hl('LspInlayHint', { fg = lighten(c.yellow, 20) })
+  end
 
   -- if you want to me to enable the highlight groups bellow, please send a screenshot for me to see how
   -- they look like or how to config for me to test.
