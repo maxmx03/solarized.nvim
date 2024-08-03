acc294b7f57777112828d0e4af86ee14969e22fd
Max Miliano <milianordelcanto@gmail.com>
2024-08-03 04:33:05 -0300

    refactor: which-key transparent bg on transparent mode.

--- lua/solarized/themes/default/whichkey.lua
+++ lua/solarized/themes/default/whichkey.lua
@@ -1,8 +1,13 @@
-return function(c)
+return function(c, config)
   local utils = require('solarized.utils')
   local set_hl = utils.set_hl
 
   set_hl('WhichKey', { fg = c.base0 }) -- the key
+  set_hl(
+    'WhichKeyNormal',
+    { fg = c.base0, bg = c.base02 },
+    { transparent = config.transparent }
+  )
   set_hl('WhichKeyGroup', { fg = c.blue }) -- a group
   set_hl('WhichKeySeparator', { fg = c.magenta }) -- the separator between the key and its label
   set_hl('WhichKeyDesc', { fg = c.base2 }) -- the label of the key
